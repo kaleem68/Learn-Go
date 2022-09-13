799e8360c7b1c635dd873f859fa2e9f902d6b543
“Kaleemullah” <nixamani5@gmail.com>
2022-09-14 03:36:05 +0500

    input / output

---
+++ hellogo.go
@@ -0,0 +1,24 @@
+package main
+import (
+    "fmt"
+    "bufio"
+    "os"
+    "log"
+)
+var pl = fmt.Println
+
+func main(){
+    readInputAndDisplayOutput()
+}
+
+func readInputAndDisplayOutput(){
+    pl("What is your name ?")  
+    reader := bufio.NewReader(os.Stdin)
+    name, err := reader.ReadString('\n')
+
+    if err == nil {
+        pl("Hello",name)
+    } else {
+        log.Fatal(err)
+    }
+}
