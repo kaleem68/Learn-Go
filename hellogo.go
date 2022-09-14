d0d7090e7296ce02d9ea91ec8814824ce3cd3046
“Kaleemullah” <nixamani5@gmail.com>
2022-09-14 18:56:51 +0500

    conditional

--- hellogo.go
+++ hellogo.go
@@ -2,54 +2,22 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"reflect"
-	"strconv"
 )
 
 var pl = fmt.Println
 
 func main() {
-	casting()
+	conditional()
 }
-func casting() {
-	cV1 := 1.7
-	cV2 := int(cV1)
-	pl(cV2)
-
-	cV3 := "342"
-	// Atoi = ASCII to integer
-	cV4, err := strconv.Atoi(cV3)
-	if err == nil {
-		pl(cV4, reflect.TypeOf(cV4))
+func conditional() {
+	iAge := 8
+	if (iAge >= 1) && (iAge <= 18) {
+		pl("Important")
+	} else if (iAge == 21) || (iAge == 50) {
+		pl("Important")
+	} else if iAge >= 65 {
+		pl("Important")
 	} else {
-		log.Fatal(err)
+		pl("Not Important")
 	}
-
-	//Itoa = Integer to ASCII
-	cv5 := strconv.Itoa(32)
-	pl(cv5, reflect.TypeOf(cv5))
-
-	//string to float
-	cV6 := "3.1415"
-	floatCV6, err2 := strconv.ParseFloat(cV6, 64)
-
-	if err2 == nil {
-		pl(floatCV6, reflect.TypeOf(floatCV6))
-	} else {
-		log.Fatal(floatCV6)
-	}
-
-	//string to float cont
-	pie := "3.14"
-	if res, err3 := strconv.ParseFloat(pie, 64); err3 == nil {
-		pl(res, reflect.TypeOf(res))
-	} else {
-		log.Fatal(err3)
-	}
-
-	//float to string: formatting similar to c
-	pieStr := fmt.Sprintf("%f", 3.14)
-	pl(pieStr, reflect.TypeOf(pieStr))
-
 }
