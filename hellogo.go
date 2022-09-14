ddfbe3c4ff4a73d9a903754a8b9fd02b712fddb3
“Kaleemullah” <nixamani5@gmail.com>
2022-09-14 23:46:01 +0500

    string length

--- hellogo.go
+++ hellogo.go
@@ -7,17 +7,9 @@ import (
 var pl = fmt.Println
 
 func main() {
-	conditional()
+	strings()
 }
-func conditional() {
-	iAge := 8
-	if (iAge >= 1) && (iAge <= 18) {
-		pl("Important")
-	} else if (iAge == 21) || (iAge == 50) {
-		pl("Important")
-	} else if iAge >= 65 {
-		pl("Important")
-	} else {
-		pl("Not Important")
-	}
+func strings() {
+	sV1 := "A word"
+	pl(len(sV1))
 }
