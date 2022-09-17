df8709a517ee31112c53975554bb755baa0800bc
“Kaleemullah” <nixamani5@gmail.com>
2022-09-17 23:46:52 +0500

    arrays basics

--- hellogo.go
+++ hellogo.go
@@ -2,97 +2,99 @@ package main
 
 import (
 	"fmt"
-	"math"
-	"math/rand"
-	"time"
 )
 
-var out = fmt.Println
+var println = fmt.Println
 
 func main() {
-	mathTest()
+	arrays()
 }
-func mathTest() {
-	out("Simple maths")
-	out("5 + 4 ", (5 + 4))
-	out("5 - 4 ", (5 - 4))
-	out("5 * 4 ", (5 * 4))
-	out("5 / 4 ", (5 / 4))
-	out("5 % 4 ", (5 % 4))
+func arrays() {
 
-	out("Increment")
+	for i := 1; i <= 10; i++ {
+		println(i)
+	}
+	println()
+
+	//iterate 10 to 1
+	for i := 10; i >= 1; i-- {
+		println(i)
+	}
+	println()
+
+	//using for loop as a while loop
+
 	count := 1
-	count++
-	count += 1
-	out(count)
+	for count <= 10 {
+		println(count)
+		count++
+	}
+	println()
 
-	//Float precision
-	out("Precise ", 0.11111111111111111111111111111111111111111111+0.11111111111111111111111111111111111111111111)
+	// ----- ARRAYS -----
+	// Collection of values with the same data type
+	// and the size can't be changed
+	// Default values are 0, 0.0, false or ""
 
-	//random number between 1 - 50
-	//since 01/01/1970
-	timeSeconds := time.Now().Unix()
-	rand.Seed(timeSeconds)
-	randomNumber := rand.Intn(50) + 1
-	out("Random digit ", randomNumber)
+	//Arrays
+	aNums := []int{1, 2, 3}
+	for index, num := range aNums {
+		println(index, num)
+	}
+	println()
+	//skip index with _
+	for _, num := range aNums {
+		println(num)
+	}
+	println()
 
-	//math functions
-	out("Abs(-10) =", math.Abs(-10))
-	out("Pow(4, 2) =", math.Pow(4, 2))
-	out("Sqrt(16) =", math.Sqrt(16))
-	out("Cbrt(8) =", math.Cbrt(8))
-	out("Ceil(4.4) =", math.Ceil(4.4))
-	out("Floor(4.4) =", math.Floor(4.4))
-	out("Round(4.4) =", math.Round(4.4))
-	out("Log2(8) =", math.Log2(8))
-	out("Log10(100) =", math.Log10(100))
-	// Get the log of e to the power of 2
-	out("Log(7.389) =", math.Log(math.Exp(2)))
-	out("Max(5,4) =", math.Max(5, 4))
-	out("Min(5,4) =", math.Min(5, 4))
+	//create and display array
+	var arr1 [5]int
+	println(arr1)
 
-	// Convert 90 degrees to radians
-	r90 := 90 * math.Pi / 180
-	// Convert 1.5708 radians to degrees
-	d90 := r90 * (180 / math.Pi)
+	println()
+	arr1[0] = 1
+	println(arr1)
 
-	fmt.Printf("%f radians = %f degrees \n", r90, d90)
+	//declear and initialize
+	arr2 := [5]int{1, 2, 3, 4, 5}
 
-	//trig
-	out(math.Sin(90))
+	//simple iteration
+	for i := 0; i < len(arr2); i++ {
+		println(arr2[i])
+	}
+	println()
+	//iterate with range
+	for i, v := range arr2 {
+		fmt.Printf("index: %d, value: %d ", i, v)
+	}
+	println()
+	//multidimensional array
+	arr3 := [2][5]int{
+		{1, 2, 3, 4, 5},
+		{6, 7, 8, 9, 10},
+	}
+	for row := 0; row < 2; row++ {
+		for col := 0; col < 5; col++ {
+			fmt.Printf("%d: ", arr3[row][col])
+		}
+		println()
+	}
 
-	// There are also functions for Cos, Tan, Acos, Asin
-	// Atan, Asinh, Acosh, Atanh, Atan2, Cosh, Sinh, Sincos
-	// Htpot
-
-	//Formatted Print, inspired by C
-
-	/*
-		%d -> Integers
-		%c -> Character
-		%f -> Floats
-		$t -> Boolean
-		%s -> String
-
-		%o -> Base 8
-		%x -> Base 16
-		%v -> Guess based on input
-
-		%T : Gives Type of supplied value
-
-	*/
-	fmt.Printf("%d, %c, %f, %t, %s, %o, %x %v \n", 21, 'A', 3.14, true, "Ahan", 21, 172, "Guess!")
-	fmt.Printf("Type: %T \n", true)
-
-	//Float formatting
-	fmt.Printf("no precision: %.f \n", 3.142)
-	fmt.Printf("round two decimal: %.2f \n", 3.142)
-
-	fmt.Printf("9 spaces: %9f \n", 3.14)
-	fmt.Printf("9 spaces 2 precision: %9.2f \n", 3.14)
-
-	// Sprintf returns a formatted string instead of printing
-	sp1 := fmt.Sprintf("%9.f", 3.14)
-	out(sp1)
+	println("Iterate 2d array")
+	//iterate 2d array with row length and col length
+	for row := 0; row < len(arr3); row++ {
+		for col := 0; col < len(arr3[row]); col++ {
+			fmt.Print(arr3[row][col], " ")
+		}
+		println()
+	}
+	println()
+	//multidimensional array without column mentioned
+	arr4 := [2][]int{
+		{1, 2, 3, 4, 5},
+		{6, 7, 8},
+	}
+	println(arr4)
 
 }
