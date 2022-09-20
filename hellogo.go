ee9bffd6eec0b606ae977b1601c15cedd49cb9ee
“Kaleemullah” <nixamani5@gmail.com>
2022-09-20 23:54:51 +0500

    map integer basics

--- hellogo.go
+++ hellogo.go
@@ -7,33 +7,12 @@ import (
 var println = fmt.Println
 
 func main() {
-	pointers()
+	mapBasics()
 }
 
-func pointers() {
-	// ----- POINTERS -----
+func mapBasics() {
+	intMap := make(map[int]int)
+	intMap[2] = 22
+	fmt.Println(intMap[2])
 
-	// You can pass by reference with the &
-	// (Address of Operator)
-	// Print amount and address for amount in memory
-	f4 := 10
-	println("f4 value ", f4)
-	println("f4 address ", &f4)
-
-	//store address of variable
-	var f4Ptr *int = &f4
-	println("f4Ptr address", f4Ptr)
-	println("f4Ptr address value", *f4Ptr)
-	*f4Ptr++
-	println("f4Ptr new address value", *f4Ptr)
-
-	pArray := [4]int{1, 2, 3, 4}
-	doubleValuesPtr(&pArray)
-	println(pArray)
-
-}
-func doubleValuesPtr(arrPtr *[4]int) {
-	for i := 0; i < 4; i++ {
-		arrPtr[i] *= 2
-	}
 }
