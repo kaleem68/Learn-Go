5a21032f72f9cf66e5742909b653086b16242d86
“Kaleemullah” <nixamani5@gmail.com>
2022-09-24 05:29:16 +0500

    struct basics example

--- hellogo.go
+++ hellogo.go
@@ -6,13 +6,32 @@ import (
 
 var println = fmt.Println
 
-func main() {
-	mapBasics()
+type Person struct {
+	name string
+	age  int
 }
 
-func mapBasics() {
-	intMap := make(map[int]int)
-	intMap[2] = 22
-	fmt.Println(intMap[2])
+func (p *Person) SetName(pName string) {
+	p.name = pName
+}
+func (p *Person) SetAge(pAge int) {
+	p.age = pAge
+}
 
+func (p *Person) GetName() string {
+	return p.name
+}
+func (p *Person) GetAge() int {
+	return p.age
+}
+
+func main() {
+	BasicStruct()
+}
+func BasicStruct() {
+	kaleem := Person{"kaleem", 22}
+	fmt.Printf("name %s, age %d \n", kaleem.GetName(), kaleem.GetAge())
+	kaleem.SetName("Kaleemullah")
+	kaleem.SetAge(23)
+	fmt.Printf("name %s, age %d \n", kaleem.GetName(), kaleem.GetAge())
 }
