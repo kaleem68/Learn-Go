dc0d9a734adfefc08e550b3a038193f92062d0e2
“Kaleemullah” <nixamani5@gmail.com>
2022-09-24 17:58:56 +0500

    interfaces implementation

--- hellogo.go
+++ hellogo.go
@@ -1,26 +1,64 @@
 package main
 
 import (
-	date "example/project/mypackage"
 	"fmt"
-	"log"
+	"reflect"
 )
 
 var println = fmt.Println
 
+type Animal interface {
+	Eat()
+	Sleep()
+}
+
+// cat
+type Cat string
+
+func (cat *Cat) Name() string {
+	return string(*cat)
+}
+
+func (cat Cat) Eat() {
+	println("Cat is eating")
+}
+
+func (cat Cat) Sleep() {
+	println("Cat is Sleep")
+}
+
+// dog
+type Dog string
+
+func (dog *Dog) Bark() {
+	fmt.Printf("%s is barking \n", dog.Name())
+}
+func (dog *Dog) Name() string {
+	return string(*dog)
+}
+func (dog Dog) Eat() {
+	println("Dog is eating")
+}
+
+func (dog Dog) Sleep() {
+	println("Dog is Sleep")
+}
+
 func main() {
-	date := date.MyDate{}
-	dateError := date.SetDay(14)
-	if dateError != nil {
-		log.Fatal(dateError)
-	}
-	monthError := date.SetMonth(1)
-	if monthError != nil {
-		log.Fatal(monthError)
-	}
-	yearError := date.SetYear(2020)
-	if yearError != nil {
-		log.Fatal(yearError)
-	}
-	println(date.Day(), date.Month(), date.Year())
+	var cat Animal = Cat("kitty")
+	cat.Eat()
+	cat.Sleep()
+	fmt.Printf("type %s \n", reflect.TypeOf(cat))
+	var newCat Cat = cat.(Cat)
+	fmt.Printf("type %s, name, %s \n", reflect.TypeOf(newCat), newCat.Name())
+
+	println()
+	var dog Animal = Dog("tom")
+	dog.Eat()
+	dog.Sleep()
+	fmt.Printf("type %s \n", reflect.TypeOf(dog))
+	var newDog Dog = dog.(Dog)
+	newDog.Bark()
+	fmt.Printf("type %s, name, %s \n", reflect.TypeOf(newDog), newDog.Name())
+
 }
