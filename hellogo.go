41276284e29cbc210b0d5a294b83188c1523aa31
“Kaleemullah” <nixamani5@gmail.com>
2022-09-24 18:32:55 +0500

    concurrency / goroutines

--- hellogo.go
+++ hellogo.go
@@ -2,63 +2,25 @@ package main
 
 import (
 	"fmt"
-	"reflect"
+	"time"
 )
 
 var println = fmt.Println
 
-type Animal interface {
-	Eat()
-	Sleep()
+func printTo15() {
+	for i := 1; i <= 15; i++ {
+		println("Fun1 ", i)
+	}
+}
+func printTo10() {
+	for i := 1; i <= 10; i++ {
+		println("Fun2 ", i)
+	}
 }
 
-// cat
-type Cat string
-
-func (cat *Cat) Name() string {
-	return string(*cat)
-}
-
-func (cat Cat) Eat() {
-	println("Cat is eating")
-}
-
-func (cat Cat) Sleep() {
-	println("Cat is Sleep")
-}
-
-// dog
-type Dog string
-
-func (dog *Dog) Bark() {
-	fmt.Printf("%s is barking \n", dog.Name())
-}
-func (dog *Dog) Name() string {
-	return string(*dog)
-}
-func (dog Dog) Eat() {
-	println("Dog is eating")
-}
-
-func (dog Dog) Sleep() {
-	println("Dog is Sleep")
-}
-
+// concurrency / go routine
 func main() {
-	var cat Animal = Cat("kitty")
-	cat.Eat()
-	cat.Sleep()
-	fmt.Printf("type %s \n", reflect.TypeOf(cat))
-	var newCat Cat = cat.(Cat)
-	fmt.Printf("type %s, name, %s \n", reflect.TypeOf(newCat), newCat.Name())
-
-	println()
-	var dog Animal = Dog("tom")
-	dog.Eat()
-	dog.Sleep()
-	fmt.Printf("type %s \n", reflect.TypeOf(dog))
-	var newDog Dog = dog.(Dog)
-	newDog.Bark()
-	fmt.Printf("type %s, name, %s \n", reflect.TypeOf(newDog), newDog.Name())
-
+	go printTo15()
+	go printTo10()
+	time.Sleep(3 * time.Second)
 }
