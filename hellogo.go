cad03b30dd352b59b6d917b3ce36a8a65c033211
“Kaleemullah” <nixamani5@gmail.com>
2022-09-27 02:32:12 +0500

    unit test

--- hellogo.go
+++ hellogo.go
@@ -2,41 +2,16 @@ package main
 
 import (
 	"fmt"
-	"sync"
-	"time"
 )
 
-var println = fmt.Println
-var printf = fmt.Printf
-
-type Account struct {
-	balance int
-	lock    sync.Mutex
-}
-
-func (a *Account) GetBalance() int {
-	a.lock.Lock()
-	defer a.lock.Unlock()
-	return a.balance
-}
-
-func (a *Account) WithDraw(amount int) {
-	a.lock.Lock()
-	defer a.lock.Unlock()
-	if amount > a.balance {
-		println("Oops not enough balance")
-	} else {
-		printf("new balance %d \n", (a.balance - amount))
-		a.balance -= amount
+func IsPositive(num int) (bool, error) {
+	if num > 0 {
+		return true, nil
 	}
+	return false, fmt.Errorf("not a positive number")
 }
 
 func main() {
-	var account Account
-	account.balance = 100
-	printf("balance %d \n", account.balance)
-	for i := 0; i < 12; i++ {
-		go account.WithDraw(10)
-	}
-	time.Sleep(2 * time.Second)
+	x := 0
+	fmt.Println("lol", float64(2/x))
 }
